3906b5124e5ebe3afa13adccb549f6f452ff7686
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-09-01 03:50:36 +0600

    almost ses

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// catch the categories
 const catchCategories = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await res.json();
     loadCategories(data?.data)
 }
+// publish the category in categories filed
 const loadCategories = (categories) => {
     const categoriesContainer = document.getElementById("categories-container");
     categories.forEach(category => {
@@ -11,15 +13,28 @@ const loadCategories = (categories) => {
         categoriesContainer.appendChild(createA);
     });
 }
+// catch the Categories Details
 const catchCategoriesDetails = async (id="1000") => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data = await res.json();
     loadCategoriesDetails(data?.data);
 }
+// publish the catchCategory Details in catchCategory Details filed
 const loadCategoriesDetails = (items) => {
-    // sortViews()
     const itemsDiv = document.getElementById("items");
     itemsDiv.textContent = "";
+    if(items.length === 0){
+        document.getElementById("items-error").textContent="";
+        const defoltitem = document.createElement("div");
+        defoltitem.classList.add = "h-screen"
+        defoltitem.innerHTML=`
+            <div class="flex mt-14 flex-col items-center text-center">
+                <img src="./Icon.png">
+                <h2 class="text-3xl font-bold">Oops!! Sorry, There is no content here</h2>
+            </div>
+        `
+        document.getElementById("items-error").appendChild(defoltitem);
+    }
     items.forEach(item =>{
         const itemDiv = document.createElement("div");
         itemDiv.classList ="";
@@ -50,6 +65,7 @@ const loadCategoriesDetails = (items) => {
         viewer(items)
     })
 }
+// try to make sort
 const viewer = (x) => {
     const items = x.sort((a, b) => parseInt(b.others.views) - parseInt(a.others.views))
     const itemsDiv = document.getElementById("items");
@@ -81,7 +97,6 @@ const viewer = (x) => {
         itemsDiv.appendChild(itemDiv);
     })
 }
-// items.sort((a, b) => parseInt(b.others.views) - parseInt(a.others.views))
 
 catchCategories()
 catchCategoriesDetails()
